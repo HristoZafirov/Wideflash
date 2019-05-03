2a0c00f560cf7ecd9c0d862f377046c1e4be5af9
Hristo Zafirov <hristozafirov96@gmail.com>
2019-05-03 16:08:54 +0300

    first commit

---
+++ js/jScript.js
@@ -0,0 +1,8 @@
+function showHomeFooter(){
+	var content = document.getElementsByClassName('content')[0];
+	content.innerHTML = '<div class="bottomLogo"><img src="images/logo/logo.png"></div><div class="margin"></div><div class="gold-line margin-top-minus5"><img src="images/logo/gold line.png"></div><div class="bottomLinks"><b>HOME</b> <b>ABOUT</b> <b>ART</b> <b>GALLERY</b> <span><b>TERMS OF SERVICE</b></span> <b>CONTACTS</b> <span><b>PRIVACY POLICY</b></span> <span><b>MY WORK</b></span></div><div class="gold-line"><img src="images/logo/gold line.png"></div>';
+}
+function showDesignFooter(){
+	var content = document.getElementsByClassName('content')[0];
+	content.innerHTML = '<div class="designs-box"><div class="rel-design right"><img src="images/design 3.jpg"><p>Living room</p></div><div class="rel-design left"><img src="images/design 2.jpg"><p>Bed room</p></div><div class="rel-design left"><img src="images/design 1.jpg"><p>Bed room</p></div><div class="clear"></div><div class="arrows"><img class="right" src="images/arrows/right arrow.png"><img class="left" src="images/arrows/left arrow.png"><div class="clear"></div></div></div>';
+}
